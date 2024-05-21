c0a5c2aec253037c3444d2e6d8c8d0015ab0e322
DEE JAY CRISTOBAL <85529829+dheejay28@users.noreply.github.com>
2024-05-21 23:48:37 +0800

    Update script.js

--- script.js
+++ script.js
@@ -24,6 +24,6 @@ function contactInfo() {
     const primaryEmail = 'deejay.cristobal@protonmail.com';
     const secondaryEmail = 'deejay.deejay.cristobal@protonmail.com';
     const emailBody = 'Hello,';
-    const email = `mailto:${primaryEmail},${secondaryEmail}?subject=Contact%20Information&body=${emailBody}`;
+    const email = `mailto:${primaryEmail},${secondaryEmail}?subject=An%20Appreciation&body=${emailBody}`;
     window.location.href = email;
 }
