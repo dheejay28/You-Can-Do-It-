631603e215d2ce410509f84fe4ed9ebcc378c67a
DEE JAY CRISTOBAL <85529829+dheejay28@users.noreply.github.com>
2024-05-21 23:01:51 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 function revealMessage() {
     document.getElementById("message").style.display = "block";
     document.getElementById("revealButton").style.display = "none";
+    document.getElementById("exitButton").style.display = "inline-block";
+    document.getElementById("contactButton").style.display = "inline-block";
+}
+
+function exitPage() {
+    document.querySelector('.container').style.display = 'none';
+}
+
+function contactInfo() {
+    const email = 'mailto:example@gmail.com?subject=Contact%20Information&body=Hello,';
+    window.location.href = email;
 }
