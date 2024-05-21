4b9b9ec24d6761c7f671ca1da2127b5b9c3bc98f
DEE JAY CRISTOBAL <85529829+dheejay28@users.noreply.github.com>
2024-05-21 23:29:05 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 function revealMessage() {
     document.getElementById("message").style.display = "block";
-    document.querySelector(".overlay-container").style.display = "none";
-    document.getElementById("exitButton").style.display = "inline-block";
-    document.getElementById("contactButton").style.display = "inline-block";
+    document.getElementById("revealButton").style.display = "none";
+    document.getElementById("buttonContainer").style.display = "block";
 }
 
 function exitPage() {
@@ -12,7 +11,7 @@ function exitPage() {
 function contactInfo() {
     const primaryEmail = 'deejay.cristobal@protonmail.com';
     const secondaryEmail = 'deejay.deejay.cristobal@protonmail.com';
-    const emailBody = '';
+    const emailBody = 'Hello,';
     const email = `mailto:${primaryEmail},${secondaryEmail}?subject=Contact%20Information&body=${emailBody}`;
     window.location.href = email;
 }
