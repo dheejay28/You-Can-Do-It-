3d2dbf593d7ba82d12fb70cfd5bc6b17f8bb7534
DEE JAY CRISTOBAL <85529829+dheejay28@users.noreply.github.com>
2024-05-21 23:55:38 +0800

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function typeMessage() {
     if (index < message.length) {
         document.getElementById("message").innerHTML += message.charAt(index);
         index++;
-        setTimeout(typeMessage, 100); // Adjust typing speed (milliseconds)
+        setTimeout(typeMessage, 150); // Adjust typing speed (milliseconds)
     } else {
         document.getElementById("contactButton").style.display = "inline-block";
     }
@@ -25,6 +25,6 @@ function contactInfo() {
     const primaryEmail = 'deejay.cristobal@protonmail.com';
     const secondaryEmail = 'deejay.deejay.cristobal@protonmail.com';
     const emailBody = 'Hello,';
-    const email = `mailto:${primaryEmail},${secondaryEmail}?subject=Contact%20Information&body=${emailBody}`;
+    const email = `mailto:${primaryEmail},${secondaryEmail}?subject=An%20Appreciation&body=${emailBody}`;
     window.location.href = email;
 }
