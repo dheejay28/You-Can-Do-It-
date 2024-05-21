1325232308eddd827d961dfa194d7b0d90d8755d
DEE JAY CRISTOBAL <85529829+dheejay28@users.noreply.github.com>
2024-05-21 23:47:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
+const message = "You can do it! Don't Give Up!";
+let index = 0;
+
 function revealMessage() {
-    document.getElementById("message").style.display = "block";
     document.getElementById("revealButton").style.display = "none";
-    document.getElementById("buttonContainer").style.display = "block";
+    document.getElementById("contactButton").style.display = "inline-block";
+    document.getElementById("message").style.display = "block";
+    typeMessage();
+}
+
+function typeMessage() {
+    if (index < message.length) {
+        document.getElementById("message").innerHTML += message.charAt(index);
+        index++;
+        setTimeout(typeMessage, 100); // Adjust typing speed (milliseconds)
+    }
 }
 
 function exitPage() {
@@ -11,7 +23,7 @@ function exitPage() {
 function contactInfo() {
     const primaryEmail = 'deejay.cristobal@protonmail.com';
     const secondaryEmail = 'deejay.deejay.cristobal@protonmail.com';
-    const emailBody = 'Hello, ';
-    const email = `mailto:${primaryEmail},${secondaryEmail}?subject=Message%20Me&body=${emailBody}`;
+    const emailBody = 'Hello,';
+    const email = `mailto:${primaryEmail},${secondaryEmail}?subject=Contact%20Information&body=${emailBody}`;
     window.location.href = email;
 }
