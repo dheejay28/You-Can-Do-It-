237f05ef0a17de1c08953770587cfd2a9f6ed5a9
DEE JAY CRISTOBAL <85529829+dheejay28@users.noreply.github.com>
2024-05-22 13:55:11 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const message = "You can do it! Don't Give Up!";
 let index = 0;
 const typingSound = document.getElementById('typingSound');
-const typingDelay = 300; // Typing delay in milliseconds to span 9 seconds
+const typingDelay = 375; // Typing delay in milliseconds to span 9 seconds
 
 function revealMessage() {
     document.getElementById("revealButton").style.display = "none";
@@ -27,19 +27,19 @@ function showMoreOptions() {
 }
 
 function exitPage() {
-    document.querySelector('.container').style.display = 'none';
+    document.querySelector('.login-container').style.display = 'none';
 }
 
 function contactInfo() {
     const primaryEmail = 'deejay.cristobal@protonmail.com';
     const secondaryEmail = 'dheejaycristobal28@gmail.com';
     const emailBody = 'Hello,';
-    const email = `mailto:${primaryEmail},${secondaryEmail}?subject=Contact%20Me&body=${emailBody}`;
+    const email = `mailto:${primaryEmail},${secondaryEmail}?subject=A%20Message&body=${emailBody}`;
     window.location.href = email;
 }
 
 function openNGL() {
-    window.open('https://ngl.com/dj28', '_blank');
+    window.open('https://ngl.link/deejay81979', '_blank');
 }
 
 function openReplies() {
@@ -66,22 +66,13 @@ function closeImageModal() {
     modal.style.display = 'none';
 }
 
-// Close modal when clicking outside of it
-window.onclick = function(event) {
-    const replyModal = document.getElementById('replyModal');
-    const imageModal = document.getElementById('imageModal');
-    if (event.target == replyModal) {
-        replyModal.style.display = 'none';
-    }
-    if (event.target == imageModal) {
-        imageModal.style.display = 'none';
-    }
-}
-
-// Function to update the Philippine time and date
 function updateDateTime() {
-    const now = new Date().toLocaleString("en-US", { timeZone: "Asia/Manila" });
-    document.getElementById('datetime').innerText = ` ${now}`;
+    const now = new Date().toLocaleString('en-PH', {
+        timeZone: 'Asia/Manila',
+        dateStyle: 'full',
+        timeStyle: 'long'
+    });
+    document.getElementById('datetime').innerText = ' ${now}`;
 }
 
 // Update the date and time every second
