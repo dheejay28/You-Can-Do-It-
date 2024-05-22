1a8eb770c2cbcf79a207e1334dbc54018b4a051d
DEE JAY CRISTOBAL <85529829+dheejay28@users.noreply.github.com>
2024-05-22 08:35:26 +0800

    Update script.js

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ function contactInfo() {
 }
 
 function openNGL() {
-    window.open('https://ngl.com/dj28', '_blank');
+    window.open('https://ngl.link/deejay81979', '_blank');
 }
 
 function openReplies() {
